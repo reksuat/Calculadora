c8fcd409c01a7e3900f1e1871c901cf63af9df32
Priscila <pyea08@gmail.com>
2024-07-07 20:20:12 -0300

    function área

---
+++ area.js
@@ -0,0 +1,15 @@
+module.exports = {
+    areaCirculo:areaCirculo,
+    areaTriangulo:areaTriangulo,
+    areaRetangulo:areaRetangulo
+};
+function areaCirculo(n1) {
+    let elevado= n1*n1
+    return pi= Math.PI * elevado
+}
+function areaTriangulo(n1,n2) {
+    return (n1*n2)/2
+}
+function areaRetangulo(n1,n2) {
+    return n1*n2
+}
