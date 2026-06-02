8a2e0e64f3748e3c070a8dd7a7fdb992b8824072
Priscila <pyea08@gmail.com>
2026-06-02 13:38:07 -0300

    feat styled buttom

---
+++ src/components/Botao.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const BotaoStyled = styled.button`
+  width: 100%;
+  background-color: ${(p) => p.theme.primaria};
+  border: none;
+  color: ${(p) => p.theme.text};
+  padding: 10px 20px;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: bold;
+  margin: 4px 2px;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: ${(p) => p.theme.secundaria};
+  }
+`;
+
+function Botao({ children, onClick }) {
+  return <BotaoStyled onClick={onClick}>{children}</BotaoStyled>;
+}
+export default Botao;
